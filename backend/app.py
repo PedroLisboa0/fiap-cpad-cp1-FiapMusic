b0f375327c8fe05ce099d70e22d7645c16d37956
Pedro Lisboa <lisboa.pedrohenrique1@gmail.com>
2026-03-23 16:58:52 -0300

    feat: update song structure

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app)
 
 PLAYLIST_FILE = os.path.join(os.path.dirname(__file__), "playlist.json")
-EXPECTED_KEYS = {"name", "artist", "floor", "likes"}
+EXPECTED_KEYS = {"name", "artist", "floor"}
 
 
 def _load_playlist():
@@ -29,11 +29,14 @@ def get_songs():
     if floor == "all":
         return jsonify(playlist), 200
     else:
-        response = [song for song in playlist if song.get("floor") == floor]
+        response = {"songs":[]}
+        for song in playlist["songs"]:
+            if song["floor"] == int(floor):
+                response["songs"].append(song)
         return jsonify(response), 200
 
 
-@app.route("/songs", methods=["POST"])
+@app.route("/songs/add_song", methods=["POST"])
 def add_song():
     song = request.json or {}
     if not EXPECTED_KEYS.issubset(song.keys()):
@@ -42,26 +45,22 @@ def add_song():
     playlist = _load_playlist()
 
     new_song = {k: str(song[k]).strip() for k in EXPECTED_KEYS}
+    new_song["likes"] = 0
     playlist.append(new_song)
     _save_playlist(playlist)
     return jsonify(new_song), 201
 
 
-@app.route("/songs/<name>", methods=["PUT"])
-def update_song(name):
-    updates = request.json or {}
-    if not updates:
-        return jsonify({"error": "Nenhuma atualização fornecida"}), 400
-
-    if not EXPECTED_KEYS.intersection(updates.keys()):
-        return jsonify({"error": f"Pelo menos uma das chaves: {EXPECTED_KEYS}"}), 400
-
+@app.route("/songs", methods=["PUT"])
+def like_song():
+    name = request.args.get("name", False) # 'Name' vem como parametro do request.
+    if not name:
+        return jsonify({"error": "Nenhuma música fornecida"}), 400
+    
     playlist = _load_playlist()
-    for song in playlist:
-        if song.get("name") == name:
-            for k in EXPECTED_KEYS:
-                if k in updates:
-                    song[k] = str(updates[k]).strip()
+    for song in playlist["songs"]:
+        if song["name"] == name:
+            song["like"] += 1
             _save_playlist(playlist)
             return jsonify(song), 200
 
